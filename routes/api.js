ef2b848c49072a064eadbb74567acb4a907d182d
Dylan Borchert <dylanbor01@gmail.com>
2023-10-13 22:47:10 -0600

    added try catch :P

--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,27 @@ require("dotenv").config();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("API is working properly");
+	res.send("API is working properly");
 });
 
 // POST sql query to database
 router.post("/db", async function (req, res, next) {
-  // get formdata from request
-  var data = req.body;
-  console.log(data);
+	try {
+		// get formdata from request
+		var data = req.body;
+		console.log(data);
 
-  if (data.query) {
-    // query database
-    var resp = await client.query(data.query);
-    res.send(resp);
-  } else {
-    res.send("No query provided");
-  }
+		if (data.query) {
+			// query database
+			var resp = await client.query(data.query);
+			res.send(resp);
+		} else {
+			res.send("No query provided");
+		}
+	} catch (error) {
+		res.send("Error connecting to database");
+		console.log(error);
+	}
 });
 
 module.exports = router;
