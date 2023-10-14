1d76cc076487e74364fd531f48869de8975cab66
Thomas Kapocsi <tkapo261@mtroyal.ca>
2023-10-14 05:25:53 -0600

    Revert "change error messsage to json"
    This reverts commit 840c2648bb68487fb93d511ec215c8ba9fb8ae39.

--- routes/api.js
+++ routes/api.js
@@ -5,27 +5,27 @@ require("dotenv").config();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("API is working properly");
+	res.send("API is working properly");
 });
 
 // POST sql query to database
 router.post("/db", async function (req, res, next) {
-  try {
-    // get formdata from request
-    var data = req.body;
-    console.log(data);
+	try {
+		// get formdata from request
+		var data = req.body;
+		console.log(data);
 
-    if (data.query) {
-      // query database
-      var resp = await client.query(data.query);
-      res.send(resp);
-    } else {
-      res.send("No query provided");
-    }
-  } catch (error) {
-    res.send("Error connecting to database");
-    console.log(error);
-  }
+		if (data.query) {
+			// query database
+			var resp = await client.query(data.query);
+			res.send(resp);
+		} else {
+			res.send("No query provided");
+		}
+	} catch (error) {
+		res.send("Error connecting to database");
+		console.log(error);
+	}
 });
 
 router.post("/bubblechart", async function (req, res, next) {
@@ -33,6 +33,7 @@ router.post("/bubblechart", async function (req, res, next) {
     // query database
     var resp = await client.bubblechart();
     res.send(resp);
+
   } catch (error) {
     res.send("Error connecting to database");
     console.log(error);
@@ -41,40 +42,42 @@ router.post("/bubblechart", async function (req, res, next) {
 
 router.post("/location", async function (req, res, next) {
   try {
-    // get formdata from request
-    var data = req.body;
-    console.log(data);
+		// get formdata from request
+		var data = req.body;
+		console.log(data);
+
+		if (data.query) {
+			// query database
+			var resp = await client.getEventsByLocation(data.query);
+			res.send(resp);
+		} else {
+			res.send("No query provided");
+		}
+	} catch (error) {
+		res.send("Error connecting to database");
+		console.log(error);
 	}
-    if (data.query) {
-      // query database
-      var resp = await client.getEventsByLocation(data.query);
-      res.send(resp);
-    } else {
-      res.send("No query provided");
-    }
-  } catch (error) {
-    res.send("Error connecting to database");
-    console.log(error);
-  }
+
 });
 
 router.post("/genre", async function (req, res, next) {
   try {
-    // get formdata from request
-    var data = req.body;
-    console.log(data);
+		// get formdata from request
+		var data = req.body;
+		console.log(data);
 
-    if (data.query) {
-      // query database
-      var resp = await client.getEventsByGenre(data.query);
-      res.send(resp);
-    } else {
-      res.send('{"error": "No query provided"}');
-    }
-  } catch (error) {
-    res.send('{"error": "Error connecting to database"}');
-    console.log(error);
-  }
+		if (data.query) {
+			// query database
+			var resp = await client.getEventsByGenre(data.query);
+			res.send(resp);
+		} else {
+			res.send("No query provided");
+		}
+	} catch (error) {
+		res.send("Error connecting to database");
+		console.log(error);
+	}
+
 });
 
 module.exports = router;
