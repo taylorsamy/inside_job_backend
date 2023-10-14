014a8a69d2a75a88390d4c635fb7ac216330b63d
Thomas Kapocsi <tkapo261@mtroyal.ca>
2023-10-13 21:34:26 -0600

    try cors

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser')
+var createError = require("http-errors");
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
-var apiRouter = require('./routes/api');
+var apiRouter = require("./routes/api");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var corsOptions = {
+  origin: "localhost:5173",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
 
+app.use(cors());
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.get('/', function(req, res, next) {
-  res.send('Node is working properly');
+app.get("/", function (req, res, next) {
+  res.send("Node is working properly");
 });
-
 
 module.exports = app;
